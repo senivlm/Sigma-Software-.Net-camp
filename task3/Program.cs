192d2fb3d40456d8c3557ef4a0ce550be13b1051
MaxChabanenko <105865006+MaxChabanenko@users.noreply.github.com>
2022-05-23 18:01:39 +0300

    Add files via upload

---
+++ task3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using task2;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+
+            try
+            {
+                Vector vec = new Vector(new int[] { 1, 3, 5, 3, 1 });
+                Console.WriteLine("Start value = \n"+vec.ToString());
+                Console.WriteLine("Is palindrome = " + vec.IsPalindrome());
+
+                Vector vec2 = new Vector(10);
+                vec2.RandomInitialization();
+                Console.WriteLine("New vector = \n" + vec2.ToString());
+                vec2.ReverseFromScratch();
+                Console.WriteLine("Reverse from scratch = \n" + vec2.ToString());
+                vec2.ReverseBuiltIn();
+                Console.WriteLine("Reverse built in = \n" + vec2.ToString());
+
+                Vector vec3 = new Vector(new int[] { 1, 3, 3, 2, 2, 2, 1, 1 });
+                Console.WriteLine("New vector = \n" + vec3.ToString());
+                Console.WriteLine("Longest subsequence = \n" + vec3.FindSubsequence().ToString());
+
+                Matrix matrix1 = new Matrix(6, 6);
+                Console.WriteLine("__Fill Diagonal Right Snake__");
+                matrix1.FillDiagonalSnake(Matrix.startAngle.Right);
+                matrix1.Output();
+
+                Matrix matrix2 = new Matrix(6, 6);
+                Console.WriteLine("__Fill Diagonal Down Snake__");
+                matrix2.FillDiagonalSnake(Matrix.startAngle.Down);
+                matrix2.Output();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+
+        }
+    }
+}
+
---
+++ task3/Vector.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Vector
+    {
+        int[] arr;
+
+
+        public int this[int index]
+        {
+            get
+            { 
+                if(index >= 0 && index < arr.Length)
+                {
+                    return arr[index];
+                }
+                else
+                {
+                    throw new Exception("Index out of range array");
+                }
+            }
+            set 
+            {
+                arr[index] = value;
+            }
+        }
+
+
+
+        public Vector(int[] arr)
+        {
+            this.arr = arr;
+        }
+
+        public Vector(int n)
+        {
+            arr = new int[n];
+        }
+
+        public Vector() { }
+
+        public void RandomInitialization(int a, int b)
+        {
+            Random random = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(a, b);
+            }
+        }
+
+        public void RandomInitialization()
+        {
+
+            Random random = new Random();
+            int x;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                while(arr[i] == 0)
+                {
+                    x = random.Next(1, arr.Length + 1);
+                    bool isExist = false;
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (x == arr[j])
+                        {
+                            isExist = true;
+                            break;
+                        }
+                    }
+                    if (!isExist)
+                    {
+                        arr[i] = x;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public Pair[] CalculateFreq()
+        {
+            
+            Pair[] pairs = new Pair[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                pairs[i] = new Pair(0,0);
+
+            }
+            int countDifference = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool isElement = false;
+                for (int j = 0; j < countDifference; j++)
+                {
+                    if(arr[i] == pairs[j].Number)
+                    {
+                        pairs[j].Freq++;
+                        isElement = true;
+                        break;
+                    }
+                }
+                if (!isElement)
+                {
+                    pairs[countDifference].Freq++;
+                    pairs[countDifference].Number = arr[i];
+                    countDifference++;
+                }
+            }
+
+            Pair[] result = new Pair[countDifference];
+            for (int i = 0; i < countDifference; i++)
+            {
+                result[i] = pairs[i];
+            }
+
+            return result;
+        }
+
+        public bool IsPalindrome()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != arr[arr.Length - i-1])
+                    return false;
+            }
+            return true;
+        }
+        public void ReverseFromScratch()
+        {
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                int temp = arr[i];
+                arr[i] = arr[arr.Length - i - 1];
+                arr[arr.Length - i - 1] = temp;
+            }
+        }
+        public void ReverseBuiltIn()
+        {
+            Array.Reverse(arr);
+        }
+        public Vector FindSubsequence()
+        {
+            int count = 0,number = 0,maxCount = 0;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] != arr[i - 1])
+                    count = 0;
+
+                count++;
+
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                    number = arr[i];
+                }
+            }
+            Vector res = new Vector(maxCount);
+            for (int i = 0; i < maxCount; i++)
+            {
+                res.arr[i] = number;
+            }
+            return res;
+        }
+        public override string ToString()
+        {
+            string str = "";
+            for (int i = 0; i < arr.Length; i++)
+            {
+                str += arr[i] + " ";
+            }
+            return str;
+        }
+    }
+}
