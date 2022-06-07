b386dbd9a10f89bd3eade016832fd6bf31fcf2a5
MaxChabanenko <105865006+MaxChabanenko@users.noreply.github.com>
2022-06-07 16:16:25 +0300

    Revision due to comments

--- task1/Classes/Check.cs
+++ task1/Classes/Check.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,11 +8,11 @@ namespace NetCamtHT_1.Classes
     {
         public static void Output(Product p)
         {
-            Console.WriteLine("Product: Name = " +  p.Name + "; Price = " + p.Price.ToString() + "; Weight = " + p.Weight.ToString());
+            Console.WriteLine(p.ToString());
         }
         public static void Output(Buy b)
         {
-            Console.WriteLine("Buy: Count = " + b.Count + "; TotalPrice = " + b.TotalPrice.ToString() + "; TotalWeight = " + b.TotalWeight.ToString());
+            Console.WriteLine(b.ToString());
         }
     }
 }
