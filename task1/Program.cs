6454ca5f54ccf6ad8b570fda70b4dbcef51bb2e5
MaxChabanenko <105865006+MaxChabanenko@users.noreply.github.com>
2022-06-07 16:14:49 +0300

    Update Program.cs

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,4 +1,4 @@
-﻿using NetCamtHT_1.Classes;
+using NetCamtHT_1.Classes;
 using System;
 
 namespace NetCamtHT_1
@@ -7,15 +7,36 @@ namespace NetCamtHT_1
     {
         static void Main(string[] args)
         {
-            Product p = new Product("Ice-cream", (decimal)23.40, (decimal)0.08);
-            Buy b = new Buy(p, 10);
-            Check.Output(p);
-            Check.Output(b);
+            Buy b = new Buy();
 
-            Product p2 = new Product("Ice-cream", (decimal)-5, (decimal)-1);
-            Buy b2 = new Buy(p2, 10);
+            Product p = new Product("Ice-cream", (double)23.40, (double)0.08);
+            Check.Output(p);
+            
+            b.Add(p);
+            Check.Output(b);
+            Console.WriteLine("_________________________");
+            Product p2 = new Product("False", (double)-5, (double)10);
             Check.Output(p2);
-            Check.Output(b2);
+
+            b.Add(p2);
+            Check.Output(b);
+            Console.WriteLine("_________________________");
+            Product p3 = new Product("Nuggets", 10, 55);
+            Check.Output(p3);
+
+            //(Element not found)
+            //b.Remove(p2);
+            //Check.Output(b);
+
+            b.Add(p3,12);
+            Check.Output(b);
+            Console.WriteLine("_________________________");
+            b.Add(p);
+            Check.Output(b);
+            Console.WriteLine("_________________________");
+            b.Remove(p);
+            Check.Output(b);
+            Console.WriteLine("_________________________");
             Console.ReadKey();
         }
     }
