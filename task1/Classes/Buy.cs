60c9fe942daf03b09a16a29a9ddc56d73e21500c
MaxChabanenko <105865006+MaxChabanenko@users.noreply.github.com>
2022-06-07 16:16:02 +0300

    Revision due to comments

--- task1/Classes/Buy.cs
+++ task1/Classes/Buy.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,34 +6,55 @@ namespace NetCamtHT_1.Classes
 {
     class Buy
     {
-        Product Product { get; }
 
-        int count;
-        public int Count
-        {
-            get {return count; }
-            set { if (value > 0) count = value; else count = 0; }
-        }
-        public decimal TotalPrice { get; }
-        public decimal TotalWeight { get; }
+        private Dictionary<Product, int> Basket;
 
-        public Buy(Product p , int c)
-        {
-            Product = p;
-            Count = c;
-            if (Product != null)
-            {
-                TotalPrice = p.Price * c;
-                TotalWeight = p.Weight * c;
-            }
-        }
+        private double TotalPrice;
+        private double TotalWeight;
         public Buy()
         {
-            if (Product != null)
+            Basket = new Dictionary<Product, int>();
+        }
+        public void Add(Product p, int c)
+        {
+            if (!Basket.ContainsKey(p))
+                Basket.Add(p, c > 0 ? c : 0);
+            else
+                Basket[p] += c;
+        }
+        public void Add(Product p)
+        {
+            if (!(p.Weight == 0 || p.Price == 0))
+                if (!Basket.ContainsKey(p))
+                    Basket.Add(p, 1);
+                else
+                    Basket[p] += 1;
+        }
+        public void Remove(Product p)
+        {
+            if (Basket.ContainsKey(p))
+                Basket.Remove(p);
+            else
+                throw new Exception("Element not found");
+        }
+        public void GetTotal()
+        {
+            TotalPrice = 0;
+            TotalWeight = 0;
+            foreach (var keyVal in Basket)
             {
-                TotalPrice = Product.Price * Count;
-                TotalWeight = Product.Weight * Count;
+                TotalPrice += keyVal.Key.Price * keyVal.Value;
+                TotalWeight += keyVal.Key.Weight * keyVal.Value;
             }
+        }
+
+        public override string ToString()
+        {
+            GetTotal();
+            string res = "Buy: Product List\n" + "TotalPrice = " + TotalPrice.ToString() + "\nTotalWeight = " + TotalWeight.ToString();
+            foreach (var keyVal in Basket)
+                res += '\n' + keyVal.Key.ToString() + "\nCount = " + keyVal.Value;
+            return res;
         }
     }
 }
