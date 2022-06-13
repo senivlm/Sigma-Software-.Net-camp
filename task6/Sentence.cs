6ff876e9cd6f502ae548c6057eb40e32e5c0aa02
senivlm <91743906+senivlm@users.noreply.github.com>
2022-06-13 11:28:15 +0300

    Update Sentence.cs

--- task6/Sentence.cs
+++ task6/Sentence.cs
@@ -9,11 +9,11 @@ namespace task6
     class Sentence
     {
         public Sentence(string path)
-        {
+        {// Ви цей об'єкт ніде не зможете застосувати.
         FileReader fr = new FileReader(path);
         }
         public string ParseText()
-        {
+        {// Де метод читання з файлу? Якась неповна версія.
             string text = ReadFromFile();
             string sent,res="";
             var firstSplit = text.Split('.', StringSplitOptions.RemoveEmptyEntries);
@@ -42,8 +42,9 @@ namespace task6
         }
 
         public string PrintAndOutputInFile()
-        {
+        {// Друк в файл не форматований. Просили таблично, тобто застосовувати до даних формат.
             string output = ParseText();
+            // Передавати як параметр
             StreamWriter writer = new StreamWriter("..\\..\\..\\Result.txt");
             writer.WriteLine(output);
             writer.Close();
@@ -51,12 +52,14 @@ namespace task6
             string res = null;
             int max, min;
             string maxWord, minWord ;
+           // Яка потреба в масиві в Split
             string[] text = output.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
             for (int i = 0;i< text.Length;i++)
             {
                 maxWord = null;
                 minWord = null;
                 max = 0;
+                // Як не хардкодити?
                 min = 100;
                 var sentArr = text[i].Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
                 foreach(var word in sentArr)
